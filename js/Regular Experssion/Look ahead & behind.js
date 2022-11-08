15b2ea9239de75170886cbabc6c3b49a31ae891d
mohammedyaseen2211 <ysn6040@gmail.com>
2022-11-08 23:11:06 +0530

    added a file

--- js/Regular Experssion/Look ahead & behind.js
+++ js/Regular Experssion/Look ahead & behind.js
@@ -1,16 +1,16 @@
 //    the first one of regular expression is look a ahead 
 
 var msg = "hello world "
-console.log(msg.match(/(l.)/g)); // this is known as assertion
+console.log(msg.match(/(l.)/g)); // this is known as assertion. this return every l elements.
 
-console.log(msg.match(/(l.) $/g)); // this dollar is used to happen to the end.
+console.log(msg.match(/(l.) $/g)); // this dollar is used to happen to the end.  this will return last element. it is also an assertion 
 
-console.log(msg.match(/(l.)(?=o)/g)); // lookahead . //question ensures it.
+console.log(msg.match(/(l.)(?=o)/g)); // lookahead . //question ensures it. it will go and search for o value after l
 
-console.log(msg.match(/(l.)(?!o)/g)); //postive expression
-
-
-console.log(msg.match(/(?<=e)(l.)/g)); // negative expreesion
+console.log(msg.match(/(l.)(?!=o)/g)); // negative expression
 
 
-console.log(msg.match(/(?<!e)(l.)/g));
+console.log(msg.match(/(?<=e)(l.)/g)); // lesser than to the before element postive expreesion
+
+
+console.log(msg.match(/(?<!e)(l.)/g));  // lesser than to the before element with negative expression
